acbc4441a316b984485413bbbfd12d8493415dbc
EvansAbraham <jea150702@outlook.com>
2025-04-13 01:20:42 +0530

    app router modification

--- client/src/app/(pages)/addengineer/page.tsx
+++ client/src/app/(pages)/addengineer/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getServerSession } from 'next-auth';
-import { options } from '@/app/api/auth/[...nextauth]';
+import { options } from '@/app/api/auth/[...nextauth]/options';
 
 export default async function AddEngineer() {
   const session = await getServerSession(options);
